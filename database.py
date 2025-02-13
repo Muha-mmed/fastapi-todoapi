bf36357fc0663ac18f74c5d0afbf8a8a8caa8bfa
Muha-mmed <mrobiu001@gmail.com>
2025-02-13 06:17:18 -0800

    changes made

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
-sessionLocal = sessionmaker(autocommit= False, autoflush=False,bind=engine)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
 
 BASE = declarative_base()
 
--- model.py
+++ model.py
@@ -7,5 +7,5 @@ class Todo(BASE):
     id= Column(Integer,primary_key=True,nullable=False)    
     title=Column(String, nullable=False)
     description = Column(String,nullable=True)
-    published = Column(Boolean, server_default='False')
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text('Now()'))
+    published = Column(Boolean, server_default=text('False'))
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
